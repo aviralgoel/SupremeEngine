fa6bd4f9a8869a0ce6688426345ce1a54142dd20
Aviral Goel <aviralgoel@rocketmail.com>
2023-02-25 18:07:53 -0800

    vec2 init

---
+++ SupremeEngine/Vec2.cpp
@@ -0,0 +1,54 @@
+#include "Vec2.h"
+
+void Vec2::add(const Vec2& v)
+{
+    
+}
+
+void Vec2::sub(const Vec2& v)
+{
+}
+
+void Vec2::scale(const float s)
+{
+}
+
+Vec2 Vec2::rotate(const float angle) const
+{
+    return Vec2();
+}
+
+float Vec2::magnitude() const
+{
+    return 0.0f;
+}
+
+float Vec2::magnitudeSquared() const
+{
+    return 0.0f;
+}
+
+Vec2& Vec2::normalize()
+{
+    // TODO: insert return statement here
+}
+
+Vec2 Vec2::unitVector() const
+{
+    return Vec2();
+}
+
+Vec2 Vec2::normal() const
+{
+    return Vec2();
+}
+
+float Vec2::dotProduct(Vec2 v) const
+{
+    return 0.0f;
+}
+
+Vec2 Vec2::crossProduct(Vec2 v) const
+{
+    return Vec2();
+}
